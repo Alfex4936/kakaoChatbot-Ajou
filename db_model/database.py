a092859f7b1bf47def3e235a0997c740014c86af
Seok Won <ikr@kakao.com>
2021-01-05 10:39:54 +0900

    Test: pool_recycle set to one hour
    As of MySQL default value for wait_timeout is 8hours and SQLAlchemy uses a default value for pool_recycle is -1,
    
    I assume that it caused the "MySQL Connection not available." error, after long inactivity.

--- db_model/database.py
+++ db_model/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.environ["KAKAO_DB"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
